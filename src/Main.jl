0f35053d3cf764c404919f988810849358b1ad72
JeffRisberg <j.s.risberg@gmail.com>
2020-12-25 21:34:20 -0800

    updated example

--- src/Main.jl
+++ src/Main.jl
@@ -14,9 +14,27 @@ for x = 1 : 1 : 10
     end
 end
 
-x = Dict("a"=>"A", "b"=>"B", "c"=>"C")
+x = Dict("a" => "A", "b" => "B", "c" => "C")
 
 for (key, value) in x
     print(key); println(value)
 end
+
+v = Vector{Float64}()
+append!(v, 0.5)
+append!(v, rand(10))
+
+for x in v
+    println(x)
+end
+
+println("Bruce Wayne"[1:5])
+
+v = ["one", "two", "three"]
+append!(v, ["four"])
+
+for x in v
+    println(x)
+end
+
 
