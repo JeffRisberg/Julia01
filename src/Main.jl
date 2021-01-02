dcccab5da32558e26827baeaa6e8ba8474c3b659
JeffRisberg <j.s.risberg@gmail.com>
2021-01-01 16:42:36 -0800

    added examples

--- src/Main.jl
+++ src/Main.jl
@@ -5,36 +5,59 @@ Main:
 - Date: 2020-12-25
 =#
 
-using Printf
+# Numbers
+count = 5
+println(typeof(count))
 
-for x = 1 : 1 : 10
-    @printf("x %i\n", x)
-    for y = 1 : 2 : 10
-        @printf("y %i\n", y)
-    end
+value = 5.5
+println(typeof(value))
+
+# Strings
+name = "Bruce Wayne"
+println(typeof(name))
+
+println(name[1:5])
+
+# Arrays
+
+v = ["one", "two", "three"]
+println(typeof(v))
+append!(v, ["four"])
+
+for x in v
+    println(x)
 end
 
+# Dictionaries
 x = Dict("a" => "A", "b" => "B", "c" => "C")
+println(typeof(x))
 
 for (key, value) in x
     print(key); println(value)
 end
 
+# Vectors
 v = Vector{Float64}()
+println(typeof(v))
+
 append!(v, 0.5)
-append!(v, rand(10))
+append!(v, rand(4))
 
 for x in v
     println(x)
 end
 
-println("Bruce Wayne"[1:5])
+# Formated output
+using Printf
 
-v = ["one", "two", "three"]
-append!(v, ["four"])
+value = 5.5
+@printf("value %f\n", value)
 
-for x in v
-    println(x)
+
+# Control Structures
+
+for x = 1 : 1 : 10
+    @printf("x %i\n", x)
 end
 
 
